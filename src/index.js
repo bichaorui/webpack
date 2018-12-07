7da958dc0661dbdc1b5c09bba89cc673e274860c
bichaorui <abcd_627360647@qq.com>
2018-12-07 16:16:28 +0800

    add eg

--- src/index.js
+++ src/index.js
@@ -2,3 +2,21 @@
 import './main.scss';
 
 console.log('run!')
+
+function component() {
+  let canvas = document.createElement('canvas');
+  function getSize() {
+    const body = document.querySelector('body');
+    canvas.height = body.offsetHeight;
+    canvas.width = body.offsetWidth;
+  }
+  window.onresize = () => {
+    getSize();
+  };
+  getSize();
+  return canvas
+}
+
+
+document.body.appendChild(component());
+
