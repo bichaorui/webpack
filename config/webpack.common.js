bbfa87ec707a458b31100df609aa7d9f0ac5c715
bichaorui <abcd_627360647@qq.com>
2019-03-16 15:18:25 +0800

    new webpack

---
+++ config/webpack.common.js
@@ -0,0 +1,74 @@
+const path = require('path');
+const CleanWebpackPlugin = require('clean-webpack-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+	entry: {
+		index: './src/index.js'
+	},
+	plugins: [
+		new CleanWebpackPlugin({
+      cleanAfterEveryBuildPatterns: ['dist']
+    }),
+		new HtmlWebpackPlugin({
+			title: '123',
+			template: path.resolve(__dirname, './../static/index.html')
+		})
+	],
+	output: {
+		filename: "[name].bundle.js",
+		chunkFilename: '[name].bundle.js',
+		path: path.resolve(__dirname, './../dist')
+	},
+	module: {
+		rules: [
+			{
+				test: /\.css$/,
+				use: [
+					'style-loader',
+					'css-loader'
+				]
+			},
+			{
+				test: /(\.jsx|\.js)$/,
+				exclude: /node_modules/
+			},
+			{
+				test: /\.scss$/,
+				use: [
+					"style-loader", // creates style nodes from JS strings
+					"css-loader", // translates CSS into CommonJS
+					"sass-loader" // compiles Sass to CSS, using Node Sass by default
+				]
+			},
+      {
+        test: /\.less$/,
+        use: [{
+          loader: "style-loader"
+        }, {
+          loader: "css-loader"
+        }, {
+          loader: "less-loader", options: {
+            javascriptEnabled: true,
+            strictMath: true,
+            noIeCompat: true
+          }
+        }]
+      },
+      {
+       test: /\.(woff|woff2|eot|ttf|otf)$/,
+       use: [
+         'file-loader'
+       ]
+      },
+			{
+				test: /\.(png|svg|jpg|gif)$/,
+				use: [
+					'file-loader'
+				]
+			}
+		]
+	}
+};
+
+
